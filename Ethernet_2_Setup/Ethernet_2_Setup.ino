7596b5aed1441de2826f0993eb24be179837457e
ewic <eric.brown@tuni.fi>
2020-04-09 16:50:44 +0300

    Ethernet module working! Main issue is bread board wiring

--- Ethernet_2_Setup/Ethernet_2_Setup.ino
+++ Ethernet_2_Setup/Ethernet_2_Setup.ino
@@ -5,15 +5,21 @@
 //    Ethernet (1) module with W5100   / RED module
 
 
+//MUST CONNECT THROUGH LAN PORT
+//NOT WA
+
+//REMOVED POTENTIOMETER
+
 
 #include <LiquidCrystal.h>                            // include LCD library
 
 #include <Ethernet2.h>             // include Ethernet libarty W5500 library
 //Ethernet2.h is for our BLUE module with the W5500 chip
 
-//#include <Ethernet.h>                 // incluide Ethernet library W5100
+//#include <Ethernet.h>    // incluide Ethernet library W5100
 
-                                // include timer library
+//timer may not be needded till MQTT client is added
+#include <TimerOne.h>      // include timer library
 
 
 //                   RS  E   D4  D5  D6  D7
@@ -28,9 +34,14 @@ void fetch_IP(void);
 /////////////////////////////////////////////////////////////////////////////////////////////  
 ///           MAC nro                                                                      //
 /// 
-#define  mac_6    0x73     ///     Last mac number  MSB mac numbers at ethernet_mega.c    ///
+#define  mac_6    0x73 //73    ///     Last mac number  MSB mac numbers at ethernet_mega.c    ///
                            //      Not relevat with Ethershield  
-static uint8_t mymac[6] = { 0x44,0x76,0x58,0x13,0x02,mac_6 };
+
+//Our modified mac addy                           
+//static uint8_t mymac[6] =  { 0x44,0x76,0x58,0x10,0x00,0x73 }; //this works!
+
+//mac address on forum
+static uint8_t mymac[6] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED }; //basic MAC address random
 
 //Original ref code mac address:
 //static uint8_t mymac[6] = { 0x44,0x76,0x58,0x10,0x00,mac_6 };
@@ -41,7 +52,7 @@ static uint8_t mymac[6] = { 0x44,0x76,0x58,0x13,0x02,mac_6 };
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///
 ///                               SETUP section
-///
+/// 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
@@ -117,6 +128,11 @@ byte rev=1;
   lcd.print("     Waiting IP     ");
 
   rev=Ethernet.begin( mymac);                  // get IP number
+
+
+  //checking rev boolean status
+  Serial.print("\nrev: ");
+  Serial.print(rev);
      
   Serial.print( F("\nW5100 Revision ") );
     
@@ -125,7 +141,7 @@ byte rev=1;
                    
      Serial.println( F( "Failed to access Ethernet controller" ) );
                    
-                                                // 0123456789012345689 
+                                 // 0123456789012345689 
      lcd.setCursor(0,0); lcd.print(" Ethernet failed   ");
   }    
                  
